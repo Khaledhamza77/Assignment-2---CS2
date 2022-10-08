fd784b61c32b9a9e7657fbfc867969d4cc03eaba
KhaledHamza <khaledhamza@aucegypt.edu>
2022-10-08 22:12:06 +0200

    getting close

--- mechanic.h
+++ mechanic.h
@@ -6,15 +6,16 @@ private:
     Appointment* bookings;
 public:
     Mechanic(int ctr=0){counter=ctr;bookings = new Appointment[counter];}
+    ~Mechanic(){delete[] bookings;}
     void set_counter(int ctr){counter=ctr;}
     int get_counter() const {return counter;}
     virtual void set_Appt(int hr, int ms){}
-    virtual Appointment get_Appt() const {return *(bookings+(counter-1));}
+    virtual Appointment get_Appt() const {return bookings[counter-1];}
     void set_Appointment(Appointment apntmnt,int index){
         counter++;
         *(bookings+index)=apntmnt;
     }
-    Appointment get_Appointment(int index) const {return *(bookings+index);}
+    Appointment get_Appointment(int index) const {return bookings[index];}
     virtual void printinfo(){
         cout<<"Mechanic's information:\n";
         cout<<"Name: "<<get_Name()<<"\n";
